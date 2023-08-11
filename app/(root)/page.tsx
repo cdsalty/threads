38e2fd1578d4eef1e74c4373e4cd532317d91fce
cdsalty <soltiscd@gmail.com>
2023-08-11 15:18:42 -0400

    initial app setup and routes

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+//app/page.tsx
+import { UserButton } from '@clerk/nextjs';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  );
+}
+
+/*
+Removed but will add back in later: <UserButton afterSignOutUrl="/" />
+
+*/
---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,33 @@
+import { SignedIn, SignOutButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Topbar = () => {
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            {/* The code within SignedIn will only show if the user is signed in.*/}
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Topbar;
