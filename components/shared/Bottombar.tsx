c45e084b0308f00bc8e24902098a684d1b5daa3f
cdsalty <soltiscd@gmail.com>
2023-08-13 23:09:27 -0400

    leftbar complete. working on bottombar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+'use client';
+import { sidebarLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const pathname = usePathname();
+  return (
+    <section className="bottombar">
+      <div className="bottombar__container">
+        {sidebarLinks.map((link, index) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar__link ${isActive && 'bg-primary-500'}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                height={24}
+                width={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Bottombar;
